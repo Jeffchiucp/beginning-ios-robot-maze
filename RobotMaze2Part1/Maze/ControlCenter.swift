bf2004981724bfee8cbd39fd8fc1b33825483d33
Jeff <jeffchiu2000@gmail.com>
2015-12-11 18:11:49 -0800

    Update ControlCenter.swift

--- RobotMaze2Part1/Maze/ControlCenter.swift
+++ RobotMaze2Part1/Maze/ControlCenter.swift
@@ -14,14 +14,24 @@ class ControlCenter {
     func moveComplexRobot(robot: ComplexRobotObject) {
         
         // Step 1.1c
-        // TODO: Call the function, isFacingWall(), and define a constant to be equal to its return value. You can use the suggested constant name below--uncomment the code and add the function call.
-        // let robotIsBlocked =
+        let robotIsBlocked = isFacingWall(myRobot, direction: myRobot.direction)
         
+        if robotIsBlocked {
+            myRobot.rotateLeft()
+        } else {
+            myRobot.move()
+        }
         // Step 1.1d
         // TODO: Test the isFacingWall() function. Be sure to comment out or delete your test code once you are finished testing!
     
         // Step 1.4
         // TODO: Write an if statement that enables the robot to choose how to move. Use the pseudocode below as a guide.
+        
+        if robotIsBlocked {
+            myRobot.rotateRight()
+        } else {
+            myRobot.move()
+        }
         
         // Pseudocode
 //         if the robot is blocked {
